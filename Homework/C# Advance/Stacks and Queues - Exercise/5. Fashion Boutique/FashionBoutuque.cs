c7f902cf9a7b52c0f921e08046d7363f9029c273
G0m0r0 <dimitrovdelyann@gmail.com>
2019-05-23 23:21:19 +0300

    exercise queue and stack

---
+++ Homework/C# Advance/Stacks and Queues - Exercise/5. Fashion Boutique/FashionBoutuque.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Fashion_Boutique
+{
+    class FashionBoutuque
+    {
+        static void Main(string[] args)
+        {
+            //stack
+            int[] clothesBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int capacityRack = int.Parse(Console.ReadLine());
+            int original = capacityRack;
+            int sumOfClothes = 0;
+            int countRacks = 1;
+
+            Stack<int> clothes = new Stack<int>(clothesBox);
+            while (clothes.Count > 0)
+            {
+                int cloth = clothes.Pop();
+
+                if (sumOfClothes + cloth < capacityRack)
+                {
+                    sumOfClothes += cloth;
+                }
+                else if (sumOfClothes + cloth == capacityRack)
+                {
+                    if (clothes.Count > 0)
+                    {
+                        countRacks++;
+                        sumOfClothes = 0;
+                    }
+                }
+                else
+                {
+                    countRacks++;
+                    sumOfClothes = 0;
+                    sumOfClothes += cloth;
+                }
+            }
+
+            Console.WriteLine(countRacks);
+        }
+    }
+}
---
+++ Homework/C# Advance/Stacks and Queues - Exercise/7. Truck Tour/TruckTour.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7._Truck_Tour
+{
+    class TruckTour
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
