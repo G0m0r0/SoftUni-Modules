cf0fa1357ce87b970ed8023624be9bdca125f6e0
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2019-10-18 18:26:53 +0300

    generics- lab finished

---
+++ Homework/C# Advance/Generics- lab/1. Box of T/BoxOfT/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> list;
+
+        public int Count => list.Count;
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        public T Remove()
+        {
+            if(list.Count==0)
+            {
+                throw new InvalidOperationException();
+            }
+            var topElement = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            return topElement;
+        }
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+    }
+}
---
+++ Homework/C# Advance/Generics- lab/3. Generic Scale/GenericScale/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var qualityScale = new EqualityScale<int>(3, 3);
+            Console.WriteLine(qualityScale.AreEqual());
+        }
+    }
+}
