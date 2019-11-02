3b6d8ff8c56fa4505cb749eda9cceb3383db9f0a
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2019-11-03 00:51:44 +0200

    abstractions- lab completed

--- Homework/OOP/Abstractions- lab/4. Hotel Reservation/StartUp.cs
+++ Homework/OOP/Abstractions- lab/4. Hotel Reservation/StartUp.cs
@@ -1,12 +1,69 @@
 ﻿using System;
 
-namespace _4._Hotel_Reservation
+namespace HotelReservation
 {
     public class StartUp
     {
-        static void Main(string[] args)
+        enum Season
         {
+            Autumn = 1,
+            Spring = 2,
+            Winter = 3,
+            Summer = 4
+        }
+        enum Disscount
+        {
+            VIP = 20,
+            SecondVisit = 10,
+            None = 0,
+        }
+        public static void Main(string[] args)
+        {
+            var tokens = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var pricePerDay = decimal.Parse(tokens[0]);
+            var numOfDays = int.Parse(tokens[1]);
+            Season season = 0;
+            switch (tokens[2])
+            {
+                case "Autumn":
+                    season = Season.Autumn;
+                    break;
+                case "Spring":
+                    season = Season.Spring;
+                    break;
+                case "Winter":
+                    season = Season.Winter;
+                    break;
+                case "Summer":
+                    season = Season.Summer;
+                    break;
+            }
+            Disscount disscount = 0;
+            if (tokens.Length == 4)
+            {
+                switch (tokens[3])
+                {
+                    case "VIP":
+                        disscount = Disscount.VIP;
+                        break;
+                    case "SecondVisit":
+                        disscount = Disscount.SecondVisit;
+                        break;
+                    case "None ":
+                        disscount = Disscount.None;
+                        break;
+                }
+            }
+            else
+            {
+                disscount = Disscount.None;
+            }
 
+
+            var totalPrice = PriceCalculator.GetTotalPrice(pricePerDay, numOfDays, (int)season, (int)disscount);
+
+            Console.WriteLine($"{(totalPrice):f2}");
         }
     }
 }
