d95a517d143a754e8cf7fed30eb46a1657d991b8
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2019-11-07 20:36:39 +0200

    inheritance- exercise

--- Homework/OOP/Inheritance- exercise/PlayersAndMonsters/StartUp.cs
+++ Homework/OOP/Inheritance- exercise/PlayersAndMonsters/StartUp.cs
@@ -1,10 +1,12 @@
-﻿namespace PlayersAndMonsters
+﻿using System;
+
+namespace PlayersAndMonsters
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-
+          
         }
     }
 }
