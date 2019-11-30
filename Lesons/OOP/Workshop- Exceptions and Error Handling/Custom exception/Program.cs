c659e9e035b192ea7ea6e42395935bc646e26ec4
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2019-11-30 15:14:08 +0200

    exception and error handling- exercise

---
+++ Lesons/OOP/Workshop- Exceptions and Error Handling/Custom exception/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Custom_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userInput = Console.ReadLine();
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw new InvalidUserInputException() 
+                { 
+                    UserInput=userInput
+                };
+            }
+        }
+    }
+    class InvalidUserInputException : Exception
+    {
+        public string UserInput { get; set; }
+        public InvalidUserInputException()
+        {
+
+        }
+
+        public InvalidUserInputException(string message)
+            :base(message)
+        {
+
+        }
+
+        public InvalidUserInputException(Exception innerException)
+            :base("Thats inner exception",innerException)
+        {
+
+        }
+    }
+}
---
+++ Lesons/OOP/Workshop- Exceptions and Error Handling/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Workshop__Exceptions_and_Error_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = int.Parse("afd");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format error");
+                //throw;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Invalid size");
+            }
+
+
+            var validator = new EgnValidator();
+            validator.IsValidEgn(null);
+
+            try
+            {
+                GenerateEng generateEgn = new GenerateEng();
+                generateEgn.GenerateEgn("19/10/1999", true, "Burgas");
+            }
+            catch (Exception ex)
+                when (ex is ArgumentException || ex is ArgumentNullException)
+            {
+                Console.WriteLine("Invalid EGN");
+                throw new Exception("Expand exception", ex);
+            }
+        }
+    }
+    public class EgnValidator
+    {
+        public bool IsValidEgn(string egn)
+        {
+            if (egn == null)
+            {
+                throw new ArgumentException(nameof(egn), "egn should not be null");
+            }
+            return true;
+        }
+    }
+
+    public class GenerateEng
+    {
+        /// <summary>
+        /// Generates valid EGN number.
+        /// </summary>
+        /// <param name="dateOfBirth"></param>
+        /// <param name="isMale"></param>
+        /// <param name="place"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">if year is less than 1900</exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        public string GenerateEgn(DateTime dateOfBirth, bool isMale, string place)
+        {
+            if (dateOfBirth.Year < 1900)
+            {
+                throw new ArgumentOutOfRangeException(nameof(dateOfBirth), "cannot generate egn before 1900");
+            }
+            if (place == null)
+            {
+                throw new ArgumentNullException(nameof(place));
+            }
+            return string.Empty;
+        }
+    }
+}
