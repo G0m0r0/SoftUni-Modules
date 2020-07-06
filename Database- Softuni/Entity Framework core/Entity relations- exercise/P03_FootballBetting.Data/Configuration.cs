524015de2f358f84043fb1d2401321f8a234049b
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2020-07-06 22:13:19 +0300

    code first  exercise miggration

---
+++ Database- Softuni/Entity Framework core/Entity relations- exercise/P03_FootballBetting.Data/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data
+{
+    internal static class Configuration
+    {
+        //.gitignore 
+        internal static string ConnectionString = @"Server=.;Database=FootballBettingSystem; Integrated security=true;";
+    }
+}
