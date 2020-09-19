b186377aba42dabe1767a60bda56d2611d6abdcb
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2020-09-19 12:22:32 +0300

    http protocol

---
+++ Web/Web basics/HTTP protocol/HTTPClientDemo/HTTPClientDemo/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace HTTPClientDemo
+{
+    using System;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class StartUp
+    {
+        static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string url = "https://softuni.bg/";
+            HttpClient httpClient = new HttpClient();
+
+            var html = await httpClient.GetStringAsync(url);
+            Console.WriteLine(html);
+        }
+    }
+}
